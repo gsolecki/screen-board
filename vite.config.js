dc7c46173e63edef96b08c90691d336043a016e1
Grzegorz Solecki <gsolecki8@gmail.com>
2025-10-28 18:13:27 -0400

    Final board

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -9,5 +9,11 @@ export default defineConfig({
     modules: {
       localsConvention: 'camelCase'
     }
-  }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  assetsInclude: ['**/*.png']
 })
