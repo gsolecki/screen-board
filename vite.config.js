04229871db5dcbff66c47ec0ecb904c1d38dc442
Grzegorz Solecki <gsolecki8@gmail.com>
2025-10-29 11:22:14 -0400

    Fix configuration issue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
+import { resolve, dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+// ESM-safe __dirname
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
